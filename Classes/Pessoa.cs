1ed01f781bc46c7e0f07de67dff9649d7a10b94c
Utilizador <Utilizador@LAPTOP-Q0SBHPJL>
2023-11-14 14:00:30 +0000

    Adicionar arquivos de projeto.

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzJerseyShop
+{
+    public class Pessoa
+    {
+        #region Construtor
+
+        /// <summary>
+        /// Construtor para Pessoa
+        /// </summary>
+        /// <param name="email">email da Pessoa</param>
+        /// <param name="nome">nome da Pessoa</param>
+        /// <param name="password">password da Pessoa</param>
+        /// <param name="numeroTelemovel">numero Telemovel da Pessoa</param>
+        public Pessoa(string email, string nome, string password, int numeroTelemovel)
+        {
+            this.Email = email;
+            this.Nome = nome;
+            this.Password = password;
+            this.NumeroTelemovel = numeroTelemovel;
+        }
+
+        #endregion
+
+
+        #region ESTADO
+
+        /// <summary>
+        /// Email de Pessoa
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Nome de Pessoa
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Password de Pessoa
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Numero Telemovel de Pessoa
+        /// </summary>
+        public int NumeroTelemovel { get; set; }
+
+        #endregion
+    }
+}
+
