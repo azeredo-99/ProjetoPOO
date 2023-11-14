c6a57970fc8c37e682bca3b17ff150db75428d06
azeredo <a23510@alunos.ipca.pt>
2023-11-14 15:46:59 +0000

    CarrinhoDeCompras

--- Classes/CarrinhoDeCompras.cs
+++ Classes/CarrinhoDeCompras.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Classes
 {
-    internal class CarrinhoDeCompras
+    public class CarrinhoDeCompras
     {
+        private List<Produto> itens = new List<Produto>();
+
+        /// <summary>
+        /// Adiciona um produto ao carrinho.
+        /// </summary>
+        /// <param name="produto">Produto a ser adicionado.</param>
+        public void AdicionarProduto(Produto produto)
+        {
+            itens.Add(produto);
+        }
+
+        /// <summary>
+        /// Exibe os itens no carrinho.
+        /// </summary>
+        public void ExibirCarrinho()
+        {
+            foreach (var produto in itens)
+            {
+                Console.WriteLine($"ID: {produto.Id}, Nome: {produto.Nome}, Preço: {produto.Preco} euros");
+            }
+        }
     }
 }
